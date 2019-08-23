6faa8df714d08868b64e1d129c2c8f152cd32652
Leon <leon.id@qq.com>
2019-08-23 11:49:17 +0800

    [Benchmark] 实现简单基准测试；

---
+++ BenchmarkDotNetTest/HashHelperTester.cs
@@ -0,0 +1,20 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNetTarget;
+
+namespace BenchmarkDotNetTest
+{
+    public class HashHelperTester
+    {
+        [Benchmark]
+        public void TestMD5()
+        {
+            HashHelper.CalcMD5("https://www.baidu.com/img/bd_logo1.png");
+        }
+
+        [Benchmark]
+        public void TestSHA1()
+        {
+            HashHelper.CalcSHA1("https://www.baidu.com/img/bd_logo1.png");
+        }
+    }
+}
--- BenchmarkDotNetTest/Program.cs
+++ BenchmarkDotNetTest/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using BenchmarkDotNet.Reports;
+using BenchmarkDotNet.Running;
 
 namespace BenchmarkDotNetTest
 {
@@ -7,7 +9,9 @@ namespace BenchmarkDotNetTest
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World! I am Test program.");
-            
+
+            Summary summary = BenchmarkRunner.Run<HashHelperTester>();
+
             Console.ReadLine();
         }
     }

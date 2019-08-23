110009d9ac878b5d143d3231bbd3a9b0668aff34
Leon <leon.id@qq.com>
2019-08-23 14:46:34 +0800

    Modified

--- BenchmarkDotNetTest/HashHelperTester.cs
+++ BenchmarkDotNetTest/HashHelperTester.cs
@@ -1,20 +1,45 @@
-﻿using BenchmarkDotNet.Attributes;
+﻿using System.Collections.Generic;
+using System.Linq;
+using BenchmarkDotNet.Attributes;
 using BenchmarkDotNetTarget;
 
 namespace BenchmarkDotNetTest
 {
+    // 测试运行时环境
+    // [CoreRtJob, MonoJob]
+    [CoreJob(baseline: true), ClrJob]
+    // 监视内存
+    [MemoryDiagnoser]
+    [RPlotExporter, RankColumn]
     public class HashHelperTester
     {
-        [Benchmark]
+        public List<string> TestTexts { get; set; } = new List<string>()
+        {
+            "",
+            "the shortest English sentence containing 26 letters",
+            string.Join("\n", Enumerable.Repeat("the shortest English sentence containing 26 letters",10))
+        };
+
+        // 使用参数源中的每个测试数据进行一次基准测试
+        [ParamsSource(nameof(TestTexts))]
+        public string TestText { get; set; }
+
+        [GlobalSetup]
+        public void Setup()
+        {
+        }
+
+        // 基准测试（作为 BaseLine）
+        [Benchmark(Baseline = true)]
         public void TestMD5()
         {
-            HashHelper.CalcMD5("https://www.baidu.com/img/bd_logo1.png");
+            HashHelper.CalcMD5(this.TestText);
         }
 
         [Benchmark]
         public void TestSHA1()
         {
-            HashHelper.CalcSHA1("https://www.baidu.com/img/bd_logo1.png");
+            HashHelper.CalcSHA1(this.TestText);
         }
     }
 }

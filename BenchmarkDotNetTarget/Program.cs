fe92fd857c77709fb3d959cef09d4422e1a9b77c
Leon <leon.id@qq.com>
2019-08-23 10:58:45 +0800

    添加项目文件。

---
+++ BenchmarkDotNetTarget/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BenchmarkDotNetTarget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! I am Target program.");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ BenchmarkDotNetTest/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BenchmarkDotNetTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! I am Test program.");
+
+            Console.ReadLine();
+        }
+    }
+}
